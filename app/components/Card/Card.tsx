bd0a62254d6b6ab8231f91f560ef68751f71b15d
leonardobaranelli <leobaranelli@gmail.com>
2023-11-03 09:21:27 -0300

    chore: step back

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import Link from 'next/link';
 import { Post } from '@/redux/features/PostSlice';
-
+//
 const Card: React.FC<Post> = (property) => {
 
   const [currentImage, setCurrentImage] = useState(0)
